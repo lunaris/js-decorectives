e07a2c6b48a0a5b6ad55884fd2a7611ed692661a
Will Jones <will@sacharissa.co.uk>
2014-02-11 21:13:39 +0000

    Play around with generalised component templates.

--- decorectives.js
+++ decorectives.js
@@ -28,4 +28,64 @@
       }
     };
   }]);
+
+  module.directive('composite', [function() {
+    var CompositeCtrl = ['$scope', '$sce', function($scope, $sce) {
+      var self = this;
+
+      var __construct = function() {
+        self.$scope = $scope.templates = {};
+      };
+
+      self.provideTemplate = function(name, compiled) {
+        self.$scope[name] = compiled;
+      };
+
+      __construct();
+    }];
+
+    return {
+      restrict: 'A',
+      transclude: true,
+      controller: CompositeCtrl,
+      link: function(scope, elem, attrs) {
+        console.log('Linking composite...');
+      },
+
+      template: '' +
+        '<div ng-transclude></div>' +
+        '<div>First: <div template="templates.first"></div></div>' +
+        '<div>Second: <div template="templates.second"></div></div>'
+    };
+  }]);
+
+  module.directive('template', [function() {
+    return {
+      restrict: 'A',
+      scope: {
+        template: '='
+      },
+      link: function(scope, elem, attrs) {
+        elem.replaceWith(scope.template);
+      }
+    };
+  }]);
+
+  module.directive('component', [function() {
+    return {
+      require: '^composite',
+      restrict: 'A',
+      transclude: true,
+      link: function(scope, elem, attrs, composite, transclude) {
+        var name = attrs['component'];
+        if (name === '') {
+          throw new Error('Component name not specified.');
+        }
+
+        transclude(function(clone) {
+          composite.provideTemplate(name, clone);
+        });
+      }
+    };
+  }]);
 })();
